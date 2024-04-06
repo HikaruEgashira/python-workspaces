d0ce5e94b2f17fd10bb4fff116fe021f49cc226d
hikae <contact@egahika.dev>
2024-04-07 03:24:03 +0900

    use vscode workspace

---
+++ demo/calc.py
@@ -0,0 +1,9 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def test_add() -> None:
+    assert add(1, 2) == 3
+
+if __name__ == "__main__":
+    print(f"{add(1, 2)=}")
--- src/calc_test.py
+++
@@ -1,5 +0,0 @@
-from src import calc
-
-
-def test_add() -> None:
-    assert calc.add(1, 2) == 3
