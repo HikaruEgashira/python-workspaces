647faa0d3dd9d6e4c487912a5ec1c6a1992c7b10
hikae <contact@egahika.dev>
2024-04-27 13:05:22 +0900

    refactor

--- langchain/summerize-web.py
+++ langchain/summerize-web.py
@@ -1,15 +1,12 @@
+import argparse
 import warnings
 
 import trafilatura
-from attr import dataclass
 from langchain.cache import SQLiteCache
 from langchain_anthropic import ChatAnthropic
 from langchain_core.globals import set_llm_cache
 from langchain_core.prompts import PromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
-
-warnings.filterwarnings("ignore")
-set_llm_cache(SQLiteCache())
 
 
 def fetch_content(url: str) -> str:
@@ -17,7 +14,6 @@ def fetch_content(url: str) -> str:
     content = trafilatura.extract(downloaded, include_links=True, output_format="json")
     if content is None:
         raise ValueError("Failed to extract content from the URL.")
-
     return content
 
 
@@ -28,7 +24,6 @@ class SummarizeOutput(BaseModel):
     Where: str = Field(description="where")
     Why: str = Field(description="why")
     How: str = Field(description="how")
-    Category: str = Field(description="academic category")
     URL: str = Field(description="url")
 
 
@@ -37,9 +32,10 @@ def summarize(content: str) -> SummarizeOutput:
     structured_llm = llm.with_structured_output(SummarizeOutput)
     prompt = PromptTemplate(
         template="""
-        task: Summarize in japanese
-        content:
-            {content}
+        ----task
+        Summarize in japanese
+        ----content
+        {content}
         """,
         input_variables=["content"],
     )
@@ -47,25 +43,19 @@ def summarize(content: str) -> SummarizeOutput:
     result = chain.invoke({"content": content})
     if not isinstance(result, SummarizeOutput):
         raise ValueError("The output is not a valid SummarizeOutput object.")
-
     return result
 
 
-@dataclass
-class Args:
-    url: str | None
+warnings.filterwarnings("ignore")
+set_llm_cache(SQLiteCache())
 
+parser = argparse.ArgumentParser(description="Webページを要約します")
+parser.add_argument("url", type=str, help="要約するWebページのURL", nargs="?")
+arg = parser.parse_args()
+if arg.url is None:
+    parser.print_help()
+    exit(0)
 
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description="Webページを要約します")
-    parser.add_argument("url", type=str, help="要約するWebページのURL", nargs="?")
-    arg = Args(**vars(parser.parse_args()))
-    if arg.url is None:
-        parser.print_help()
-        exit(0)
-
-    content = fetch_content(arg.url)
-    summary = summarize(content)
-    print(summary.json(indent=4, ensure_ascii=False))
+content = fetch_content(arg.url)
+summary = summarize(content)
+print(summary.json(indent=4, ensure_ascii=False))
